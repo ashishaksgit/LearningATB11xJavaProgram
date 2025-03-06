0394932f6b3fc8036da621a88694f4a1b7bcae6e
Ashish Sharma <ashish.aks2006@gmail.com>
2025-03-06 18:07:26 +0530

    2nd HelloWorld program added

---
+++ src/ex_01_Java_Basic/HelloWorld1.java
@@ -0,0 +1,11 @@
+package ex_01_Java_Basic;
+
+import com.sun.security.jgss.GSSUtil;
+
+public class HelloWorld1 {
+    public static void main(String[] args)
+    {
+        System.out.println("Helloo, World!");
+        System.out.println("Ashish Kumar Sharma ");
+    }
+}
