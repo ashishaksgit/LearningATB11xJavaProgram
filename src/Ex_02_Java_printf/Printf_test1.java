5de43ff84cda005ace990d3eb8fb0ec301cd2189
Ashish Sharma <ashish.aks2006@gmail.com>
2025-03-11 20:54:08 +0530

    first exercise of printf

---
+++ src/Ex_02_Java_printf/Printf_test1.java
@@ -0,0 +1,19 @@
+package Ex_02_Java_printf;
+
+public class Printf_test1 {
+    public static void main(String[] args)
+    {
+        int table = 9;
+
+        System.out.printf("%d × %d = %d%n", table, 1, table * 1);
+        System.out.printf("%d × %d = %d%n", table, 2, table * 2);
+        System.out.printf("%d × %d = %d%n", table, 3, table * 3);
+        System.out.printf("%d × %d = %d%n", table, 4, table * 4);
+        System.out.printf("%d × %d = %d%n", table, 5, table * 5);
+        System.out.printf("%d × %d = %d%n", table, 6, table * 6);
+        System.out.printf("%d × %d = %d%n", table, 7, table * 7);
+        System.out.printf("%d × %d = %d%n", table, 8, table * 8);
+        System.out.printf("%d × %d = %d%n", table, 9, table * 9);
+        System.out.printf("%d × %d = %d%n", table, 10, table * 10);
+    }
+}
---
+++ src/ex_01_Java_Basic/Commentspractice.java
@@ -0,0 +1,12 @@
+package ex_01_Java_Basic;
+
+public class Commentspractice {
+    public static void main(String[] args) {
+        //single line
+        /*System.out.println("congratulations");
+        System.out.println("congratulations");
+        System.out.println("congratulations"); */
+        System.out.println("congratulations");
+        System.out.println("congratulations");
+    }
+}
