1bf9242ce5278af1c3222a55ddbcf4887ac5d681
Ashish Sharma <ashish.aks2006@gmail.com>
2025-03-12 16:46:26 +0530

    Merge remote-tracking branch 'origin/master'

---
+++ src/Ex_03_Java_12_03_2025/Exercise2.java
@@ -0,0 +1,10 @@
+package Ex_03_Java_12_03_2025;
+
+public class Exercise2 {
+    static char myChar;
+    public static void main(String[] args) {
+      //  char mychar;
+        System.out.println("Default char value: [" + myChar + "]"); // Output: []
+        System.out.println("Numeric value: " + (int) myChar);
+    }
+}
