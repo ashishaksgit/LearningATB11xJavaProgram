5005a0df64ed5e176971bf6546a2bbf7d4bb6553
Ashish Sharma <ashish.aks2006@gmail.com>
2025-03-13 14:45:49 +0530

    Merge remote-tracking branch 'origin/master'

---
+++ src/ex_01_Java_Basic/Lab005_Comments.java
@@ -0,0 +1,9 @@
+package ex_01_Java_Basic;
+// Author - Ashish Kumar Sharma
+// Program Details - Hello World
+public class Lab005_Comments {
+    public static void main(String[] args) {
+        // This is also a comment
+        System.out.println("Hell World!");
+    }
+}
---
+++ src/ex_01_Java_Basic/Lab006_Comments_Multi.java
@@ -0,0 +1,13 @@
+package ex_01_Java_Basic;
+
+public class Lab006_Comments_Multi {
+    public static void main(String[] args) {
+        /**
+         * Author : Ashish Kumar Sharma
+         * Course :  ATB11x
+         * Learning :  Java
+         *
+         **/
+        System.out.println("Multi Comment");
+    }
+}
