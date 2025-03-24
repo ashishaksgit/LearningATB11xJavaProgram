434dfad440090d1a20b385a29f3e2311f918300d
Ashish Sharma <ashish.aks2006@gmail.com>
2025-03-24 22:29:36 +0530

    Merge remote-tracking branch 'origin/master'

---
+++ src/ex_06_Ternary_Operator/Lab062_TO.java
@@ -0,0 +1,11 @@
+package ex_06_Ternary_Operator;
+
+public class Lab062_TO {
+    public static void main(String[] args) {
+        // ? :
+        //This Java program demonstrates the ternary operator (? :), which is a shorthand for if-else statements.
+        int age = 19;
+        String canIVote  =  age >=18 ? "Yes, You can Vote" : "No You can't Vote";
+        System.out.println(canIVote);
+    }
+}
