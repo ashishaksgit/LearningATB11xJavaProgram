cd1c385e79d55943debe16fe89362d318f35ade4
Ashish Sharma <ashish.aks2006@gmail.com>
2025-05-23 15:24:48 +0530

    Merge remote-tracking branch 'origin/master'

---
+++ src/Ashish_Java_Practice/Wrapperclass/Mobile1.java
@@ -0,0 +1,26 @@
+package Ashish_Java_Practice.Wrapperclass;
+
+public class Mobile1 {
+    private  Integer phone1;
+    private String name1;
+    Mobile1()
+    {
+        System.out.println("DC");
+    }
+
+    public Integer getPhone1() {
+        return phone1;
+    }
+
+    public void setPhone1(Integer phone1) {
+        this.phone1 = phone1;
+    }
+
+    public String getName1() {
+        return name1;
+    }
+
+    public void setName1(String name1) {
+        this.name1 = name1;
+    }
+}
