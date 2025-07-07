c291aa24be3872c2f3792c56e88997223128f72f
Ashish <ashish.aks2006@gmail.com>
2025-07-07 21:41:32 +0530

    file modified

--- src/Ashish_Java_Practice/Coding_Interview_Questions/Table_Print.java
+++ src/Ashish_Java_Practice/Coding_Interview_Questions/Table_Print.java
@@ -4,7 +4,8 @@ public class Table_Print {
     public static void main(String[] args) {
         int n = 5;
         for (int i = 1; i <= 10; i++) {
-            System.out.printf("%d x %d = %d\n", i, n, i * n);//Ashish
+            System.out.printf("%d x %d = %d\n", i, n, i * n);//Ashishlg
+
 
             //System.out.println(i*n);
         }
