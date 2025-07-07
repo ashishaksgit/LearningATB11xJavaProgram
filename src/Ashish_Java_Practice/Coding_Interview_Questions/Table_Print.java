84571ac67ff86be5cd80e3989d0dd104c08cc768
Ashish Sharma <ashish.aks2006@gmail.com>
2025-07-07 20:22:19 +0530

    Code pushed from ashish

---
+++ src/Ashish_Java_Practice/Coding_Interview_Questions/Table_Print.java
@@ -0,0 +1,12 @@
+package Ashish_Java_Practice.Coding_Interview_Questions;
+
+public class Table_Print {
+    public static void main(String[] args) {
+        int n = 5;
+        for (int i = 1; i <= 10; i++) {
+            System.out.printf("%d x %d = %d\n", i, n, i * n);
+
+            //System.out.println(i*n);
+        }
+    }
+}
