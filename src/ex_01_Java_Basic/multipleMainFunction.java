eb372f3849fac513bbc7f7a46cc0b9b810cc2185
Ashish <ashish.aks2006@gmail.com>
2025-07-17 20:51:57 +0530

    Code pushed from ashish

---
+++ src/ex_01_Java_Basic/multipleMainFunction.java
@@ -0,0 +1,17 @@
+package ex_01_Java_Basic;
+
+public class multipleMainFunction {
+    public static void main(String[] args) {
+        System.out.println("main1");
+    }
+
+    public static void main(String args) {
+        System.out.println("Main2");
+
+    }
+
+    public static void main(int args) {
+        System.out.println("main3");
+
+    }
+}
