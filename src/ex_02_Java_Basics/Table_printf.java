6141caa6d29d5932f604f17b2be321f092261a5c
Ashish <ashish.aks2006@gmail.com>
2025-07-23 23:33:51 +0530

    Code pushed from ashish

---
+++ src/ex_02_Java_Basics/Table_printf.java
@@ -0,0 +1,18 @@
+package ex_02_Java_Basics;
+
+public class Table_printf {
+    public static void main(String[] args) {
+        int num = 9;
+        System.out.println("Multiplication Table of " +num+":");
+        System.out.printf("%d*1=%d%n" ,num,(num));
+        System.out.printf("%d*2=%d%n",num,(num*2));
+        System.out.printf("%d*3=%d%n",num,(num*3));
+        System.out.printf("%d*4=%d%n",num,(num*4));
+        System.out.printf("%d*5=%d%n",num,(num*5));
+        System.out.printf("%d*6=%d%n",num,(num*6));
+        System.out.printf("%d*7=%d%n",num,(num*7));
+        System.out.printf("%d*8=%d%n",num,(num*8));
+        System.out.printf("%d*9=%d%n",num,(num*9));
+        System.out.printf("%d*10=%d%n",num,(num*10));
+    }
+}
